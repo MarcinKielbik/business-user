17c21c4d552527c290a775f8c3f3244b294ba601
MarcinKielbik <marcinlukasz.kelbik@gmail.com>
2026-01-12 11:32:29 +0100

    Add emit info toast

--- src/app/services/toast.service.spec.ts
+++ src/app/services/toast.service.spec.ts
@@ -13,4 +13,17 @@ describe('ToastService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should emit info toast', () => {
+    service.stream$.subscribe(toast => {
+      expect(toast.type).toBe('info');
+      expect(toast.message).toBe('Test info');
+      expect(toast.timeout).toBe(4000);
+      expect(toast.id).toBe(1);
+
+      service.info('Test info');
+
+    })
+  });
+
 });
